1d0272f5928ee653cf7b955cbef616adbf72ab2d
VasoBechvaia <supersuperadze1@gmail.com>
2022-09-21 17:56:15 +0400

    first commit

---
+++ group 2/davaleba 2/davaleba.py
@@ -0,0 +1,10 @@
+name = "Vaso"
+surname = "Bechvaia"
+height = 168
+age = 16
+school = 178
+knows_programing = False
+is_ugly = False
+
+
+print(name + " " + surname + " " + str(height) + " " + str(age)  + " " + str(school) + " " + str(knows_programing) + " " + str(is_ugly)   )
